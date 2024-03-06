026c314b01717a3cdde51c0062e8e994fd7a9ca5
Davidwkk <david20030701@gmail.com>
2024-03-06 23:28:11 +0800

    finished lab2-1

--- CSCI2100/Lab2/1sol.c
+++ CSCI2100/Lab2/1sol.c
@@ -59,7 +59,10 @@ int IsFull(StackADT s) {
 // Tips: If it is full, do nothing.
 void Push(StackADT s, int value) {
     // write your code here
-
+    if(!IsFull(s)){
+        s->arr[s->size] = value;
+        s->size++;
+    }
 }
 
 // Description: Pop a value from the stack
@@ -67,7 +70,9 @@ void Push(StackADT s, int value) {
 // Tips: If it is empty, do nothing.
 void Pop(StackADT s) {
     // write your code here
-
+    if(!IsEmpty(s)){
+        s->size--;
+    }
 }
 
 // Description: Obtain the top value from the stack
@@ -75,7 +80,12 @@ void Pop(StackADT s) {
 // Tips: Your should check if it is empty. If empty, you should return INF.
 int Top(StackADT s) {
     // write your code here
-
+    if(IsEmpty(s)){
+        return INF;
+    }
+    else{
+        return s->arr[s->size-1];
+    }
 }
 
 // Description: Input management
